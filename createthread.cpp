0b1a4b0addc713ed5d1ff8a90d302c1a942b6b3c
sudeshna-lab <62993714+sudeshna-lab@users.noreply.github.com>
2026-05-12 12:45:50 +0530

    Create createthread.cpp

---
+++ createthread.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<thread>
+
+using namespace std;
+
+void func(int x)
+{
+    while(x --> 0)
+    {
+        cout << x << endl;
+    }
+}
+
+ auto fun = [](int x)
+ {
+     while(x--> 0)
+     {
+         cout << x << endl;
+     }
+ };
+
+int main()
+{
+    
+    std::thread t1(func, 10);
+   std::thread t2(fun, 5);
+    
+    t1.join();
+    t2.join();
+    
+}
+
